e9028f8d50a3d6a5a9fea1dda93a8406fbdb1bbd
Muhammad Yazid Akbar <yazidakbar08@gmail.com>
2023-11-25 20:40:19 +0700

    update cik

--- script.js
+++ script.js
@@ -5,9 +5,8 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 18th!";
-  gif.src =
-    "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+  question.innerHTML = "Aaww Love You Too <3";
+  gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
 
 noBtn.addEventListener("mouseover", () => {
